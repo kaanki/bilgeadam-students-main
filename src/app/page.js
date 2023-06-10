105418c9f08f38bcdb5944a32f44887c3c0ab73b
TBicer <tunaybicer99@gmail.com>
2023-06-10 22:53:47 +0300

    Creating Layout Of The Site

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import HeroContainer from '@/components/HeroContainer';
+import MenuContainer from '@/components/MenuContainer';
+
+export default function Home() {
+  return (
+    <>
+      <MenuContainer />
+      <HeroContainer />
+    </>
+  );
+}
